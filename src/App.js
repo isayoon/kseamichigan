c1679f61196fe37e1bc8d105e0e79e024bbf3023
Peter.S <nsaspetersong@gmail.com>
2023-12-22 03:13:35 -0500

    correct Homepage file structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+      <HomePage></HomePage>
       </header>
     </div>
   );
---
+++ src/components/HomePage.js
@@ -0,0 +1,11 @@
+import './HomePage.css';
+
+function HomePage() {
+    return (
+      <div>
+        <h1 class = "title">Korean American Scientists & Engineers Association</h1>
+      </div>
+    );
+  }
+  
+  export default HomePage;
