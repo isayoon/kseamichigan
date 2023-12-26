e1a61a03e2e0879487cf1e80b388331702589d5f
Peter.S <nsaspetersong@gmail.com>
2023-12-26 10:53:02 -0500

    vertical line

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,26 +4,27 @@ import Ksealogo from "../images/ksealogo.png";
 
 function HomePage() {
   return (
-    <div className = "HomePageWrapper">
-    <header className="MenuBar">
-      <img src={Ksealogo} alt="KSEA logo" className="logo" />
-      <p id="topKsea">KSEA</p>
-      <div className="vertical-line"></div>
-      <nav className="navigation">
+    <div className="HomePageWrapper">
+      <header className="MenuBar">
+        <img src={Ksealogo} alt="KSEA logo" className="logo" />
+        <p id="topKsea">KSEA</p>
+        <div className="vertical-line"></div>
+        <p className="umich">University of Michigan</p>
+        <nav className="navigation">
           <a href="/about">About Us</a>
           <a href="/events">Events</a>
           <a href="/alumni">Alumni</a>
           <a href="/committees">Committees</a>
           <a href="/join">Join</a>
         </nav>
-    </header>
+      </header>
 
-    <main className="main-content">
-      <h1 className="title">
-        Korean American Scientists & Engineers Association
-      </h1>
-      <button className="register-button">Register Here</button>
-    </main>
+      <main className="main-content">
+        <h1 className="title">
+          Korean American Scientists & Engineers Association
+        </h1>
+        <button className="register-button">Register Here</button>
+      </main>
     </div>
   );
 }
