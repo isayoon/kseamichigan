f5f8c85804cab831d698f2efed6ca6d6d6ac74a6
Peter.S <nsaspetersong@gmail.com>
2023-12-26 03:08:21 -0500

    header

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,31 @@
-import './HomePage.css';
+import React from "react";
+import "./HomePage.css";
+import Ksealogo from "../images/ksealogo.png";
 
 function HomePage() {
-    return (
-      <div className="homepage">
-        <hr></hr>
-        <h1 className = "title">Korean American Scientists & Engineers Association</h1>
-      </div>
-    );
-  }
-  
-  export default HomePage;
+  return (
+    <div className = "HomePageWrapper">
+    <header className="MenuBar">
+      <img src={Ksealogo} alt="KSEA logo" className="logo" />
+      <p id="topKsea">KSEA</p>
+      <div className="vertical-line"></div>
+      <nav className="navigation">
+          <a href="/about">About Us</a>
+          <a href="/events">Events</a>
+          <a href="/alumni">Alumni</a>
+          <a href="/committees">Committees</a>
+          <a href="/join">Join</a>
+        </nav>
+    </header>
+
+    <main className="main-content">
+      <h1 className="title">
+        Korean American Scientists & Engineers Association
+      </h1>
+      <button className="register-button">Register Here</button>
+    </main>
+    </div>
+  );
+}
+
+export default HomePage;
