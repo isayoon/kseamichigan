841ac2c5fdb46adab9197a4f8abb0d7270b17bb0
Seung Whan (Peter) Song <75101190+Pikklefish@users.noreply.github.com>
2024-05-29 23:09:20 -0400

    started nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
         colors:{
             'bg1-color': '#FCFAF4',
             'textblue-color': '#3954A5',
-        }
+        },
+        fontFamily: {
+            custom: ['Archivo_Black', 'sans-serif'],
+            custom1: ['Outfit_bold', 'sans-serif'],
+          },
     },
   },
   plugins: [],
